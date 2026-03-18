94727a6c0034138313e1d63c3f425482e765b4dd
Cham Manganti <chammanganti@gmail.com>
2026-03-18 13:28:43 +0800

    feat: remove dynamodb

--- terraform/local/cluster/docker.tf
+++ terraform/local/cluster/docker.tf
@@ -17,36 +17,3 @@ resource "docker_container" "registry" {
         name = docker_network.kind.name
     }
 }
-
-resource "docker_volume" "dynamodb_data" {
-    name = "dynamodb"
-}
-
-resource "docker_container" "dynamodb" {
-    name = "dynamodb"
-    image = "amazon/dynamodb-local:latest"
-    restart = "always"
-
-    command = [
-        "-jar", "DynamoDBLocal.jar",
-        "-sharedDb",
-        "-dbPath", "."
-    ]
-
-    ports {
-        internal = 8000
-        external = 8000
-    }
-
-    mounts {
-        target = "/home/dynamodblocal/data"
-        source = docker_volume.dynamodb_data.name
-        type = "volume"
-    }
-
-    working_dir = "/home/dynamodblocal"
-
-    networks_advanced {
-        name = docker_network.kind.name
-    }
-}
